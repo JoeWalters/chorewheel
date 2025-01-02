12bc066644961206b86115c8b386a7d06750b60a
Joseph Walters <JoeWalters.email@gmail.com>
2025-01-02 14:36:17 -0600

    Can change port

--- app.py
+++ app.py
@@ -153,4 +153,4 @@ def home():
     return render_template_string(HTML_TEMPLATE, tasks=tasks)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
